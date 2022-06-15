a4b9531da5733811b7cc53554f04c8444c0b9828
Gabriel Alencar <gfalencar02@gmail.com>
2022-06-15 12:51:49 -0300

    First Commit

---
+++ src/test/java/com/gfalencar/ProjetoRest/ProjetoRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gfalencar.ProjetoRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
